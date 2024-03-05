79583972af4534f96ed221677a20ed579db4c6b0
colin <csn23@ic.ac.uk>
2024-03-05 11:24:21 +0000

    added loading spinner for backend calls

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -23,6 +23,7 @@ function SignupForm() {
 
   // Setup errors
   const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
   // Setup states for fetching degrees and degreelevels
   const [degrees, setDegrees] = useState([]);
@@ -192,7 +193,7 @@ function SignupForm() {
                   for="email"
                   class="row-span-2 col-span-1 h-6 font-medium text-gray-700 text-xl pt-0.5 mr-2 mb-2 pl-0.5 pr-5"
                 >
-                  Email:
+                  School Email:
                 </label>
 
                 <input
@@ -351,12 +352,12 @@ function SignupForm() {
                     onChange={handleChange}
                   />
                   <span className="font-medium text-xl ml-5 w-36">
-                    Internationl Student
+                    International Student
                   </span>
                 </label>
               </div>
               <div className="row-span-1 col-span-3 h-6 flex flex-col">
-                <label class="flex flex-col w-[80%]">
+                <label class="flex flex-col w-[90%]">
                   <span className="font-medium text-xl mb-1">Degree Level</span>
                   <select
                     className="text-gray-700 text-xl pt-0.5 border-2  border-gray-600 rounded-md pl-2"
@@ -383,8 +384,8 @@ function SignupForm() {
                   </p>
                 )}
               </div>
-              <div className="row-span-1 col-span-1 h-6 flex flex-col">
-                <label class="flex flex-col w-[80%]">
+              <div className="row-span-1 col-span-1 h-6 flex flex-col ml-2">
+                <label class="flex flex-col w-28">
                   <span className="font-medium text-xl mb-1">Start Year</span>
                   <select
                     className="text-gray-700 text-xl pt-0.5 border-2  border-gray-600 rounded-md pl-2"
@@ -392,9 +393,7 @@ function SignupForm() {
                     onChange={handleChange}
                     name="startYear"
                   >
-                    <option value="" disabled>
-                      Select a year
-                    </option>
+                    <option value="" disabled></option>
                     {startYears.map((year) => (
                       <option key={year} value={year}>
                         {year}
@@ -408,8 +407,8 @@ function SignupForm() {
                   </p>
                 )}
               </div>
-              <div className="row-span-1 col-span-1 h-6 flex flex-col">
-                <label class="flex flex-col w-[80%] ml-12">
+              <div className="row-span-1 col-span-1 h-6 flex flex-col ml-6">
+                <label class="flex flex-col ml-6 w-28">
                   <span className="font-medium text-xl mb-1">End Year</span>
                   <select
                     className="text-gray-700 text-xl pt-0.5 border-2  border-gray-600 rounded-md pl-2"
@@ -417,9 +416,7 @@ function SignupForm() {
                     onChange={handleChange}
                     name="endYear"
                   >
-                    <option value="" disabled>
-                      Select a year
-                    </option>
+                    <option value="" disabled></option>
                     {endYears.map((year) => (
                       <option key={year} value={year}>
                         {year}
@@ -459,12 +456,38 @@ function SignupForm() {
               </div>
             </div>
             <div className="flex justify-end">
-              <button
-                type="submit"
-                className="bg-[#eb580a]  hover:bg-[#ff9f4b] text-white font-bold py-1 px-4 mr-12  mt-2 h-9 rounded-md"
-              >
-                Sign Up
-              </button>
+              {loading ? (
+                <button
+                  disabled
+                  type="submit"
+                  className="bg-[#ff9f4b] text-white font-bold py-1 px-4 mr-8 w-28 mt-2 h-9 rounded-md flex items-center space-x-1"
+                >
+                  <span>Loading </span>
+                  <svg
+                    aria-hidden="true"
+                    class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="bg-[#eb580a]  hover:bg-[#ff9f4b] text-white font-bold py-1 px-4 mr-8 w-28 mt-2 h-9 rounded-md"
+                >
+                  Sign Up
+                </button>
+              )}
             </div>
           </form>
         </div>
